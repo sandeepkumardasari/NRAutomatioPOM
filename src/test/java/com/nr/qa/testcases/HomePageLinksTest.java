2011bf3d4699a4ce88c19aea0cc2bb95b708a864
Sandeep Dasari <sandyneo24@gmail.com>
2020-10-13 16:23:57 +0530

    Login

--- src/test/java/com/nr/qa/testcases/HomePageLinksTest.java
+++ src/test/java/com/nr/qa/testcases/HomePageLinksTest.java
@@ -1,6 +1,5 @@
 package com.nr.qa.testcases;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,6 +24,7 @@ public class HomePageLinksTest extends TestBase{
 		initialization();
 		loginPage=new AEMLoginPage();
 		loginPage.Login();
+		System.out.println("Login Successful");
 	}
 	
 	@Test
